69e967d93e10b200542f4351c7f19a1ce28670bf
banzayche <dharm.serg@gmail.com>
2016-04-18 14:11:02 +0300

    initial commit

---
+++ public/js/routes.js
@@ -0,0 +1,25 @@
+angular.module('NoteWrangler').config(function($routeProvider){
+	$routeProvider
+		.when('/', {
+			redirectTo: '/notes'
+		})
+		.when('/notes', {
+			templateUrl: '/public/templates/notes-index.html',
+			controller: 'NotesIndexController'
+		})
+		
+		.when('/notes/new', {
+			templateUrl: '/public/templates/notes-new.html',
+			controller: 'NotesCreateController'
+		})
+		
+		.when('/notes/:id', {
+			templateUrl: '/public/templates/notes-item.html',
+			controller: 'NotesShowController'
+		})
+		
+		.when('/notes/:id/edit', {
+			templateUrl: '/public/templates/notes-edit.html',
+			controller: 'NotesEditController'
+		})
+});
